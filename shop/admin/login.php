a35128bd0ffa48314122e5b12c19fef760af9298
Samitha Wickramasinghe <contact.samitha@gmail.com>
2024-09-26 20:05:19 +0530

    IT21568732

--- shop/admin/login.php
+++ shop/admin/login.php
@@ -1,5 +1,24 @@
 <?php include '../classess/Adminlogin.php';?>
 <?php
+
+//IT21568732
+ // Enable a Content Security Policy (CSP) header
+ header("Content-Security-Policy: default-src 'self'; frame-ancestors 'none'");
+ // Set X-Content-Type-Options header to 'nosniff'
+ header("X-Content-Type-Options: nosniff");
+
+ // Remove or suppress the X-Powered-By header
+ header_remove("X-Powered-By");
+ header_remove("Server");
+
+ session_start(
+	 [
+		 'cookie_httponly' => true,  // Set the HttpOnly flag
+		 'cookie_samesite' => 'Lax', // Set to 'Strict' if needed
+	 ]
+ );
+
+
 $al = new Adminlogin();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$adminUser = $_POST['adminUser'];
